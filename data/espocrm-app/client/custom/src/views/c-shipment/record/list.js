77e21dd38192eeded735d729ee917fa2160f6044
NhatTranANHP <nhattrananhp@gmail.com>
2026-04-15 11:20:16 +0700

    git update Cshipment search

--- data/espocrm-app/client/custom/src/views/c-shipment/record/list.js
+++ data/espocrm-app/client/custom/src/views/c-shipment/record/list.js
@@ -1,117 +1,108 @@
 define('custom:views/c-shipment/record/list', ['views/record/list'], function (Dep) {
 
-  return Dep.extend({
+    return Dep.extend({
+        // template: 'c-shipment/record/list',
 
-    massActionList: ['remove', 'merge', 'massUpdate', 'export'],
+        massActionList: ['remove', 'merge', 'massUpdate', 'export'],
+        checkAllResultMassActionList: ['remove', 'massUpdate', 'export'],
 
-    checkAllResultMassActionList: ['remove', 'massUpdate', 'export'],
+        setup: function () {
+            Dep.prototype.setup.call(this);
 
-    setup: function () {
-      Dep.prototype.setup.call(this);
-    },
+            // Bật một thông báo để chắc chắn file này đang chạy phiên bản mới nhất
+            // console.log("--- List View Custom Loaded Successfully ---");
 
-    afterRender: function () {
-      Dep.prototype.afterRender.call(this);
+            // CAN THIỆP VÀO COLLECTION FETCH
+            // Đây là cách duy nhất để sửa URL API khi các hàm View bị bỏ qua
+            var self = this;
+            var originalFetch = this.collection.fetch.bind(this.collection);
 
-      // Ajouter le bouton Export by Date Range
-      this.addExportButton();
-    },
+            this.collection.fetch = function (options) {
+                // console.log("--- Đang chặn đứng request API ---");
+                
+                if (this.where && this.where.length > 0) {
+                    // console.log("Dữ liệu lọc gốc:", JSON.stringify(this.where));
 
-    addExportButton: function () {
-      // Trouver le conteneur des boutons header
-      const $header = this.$el.find('.list-buttons-container');
+                    // Biến đổi cấu trúc
+                    this.where = this.where.map(function (item) {
+                        if (item.type === 'textFilter') {
+                            // console.log("Phát hiện textFilter:", item.value);
+                            return {
+                                type: 'and',
+                                value: [
+                                    {
+                                        type: 'contains',
+                                        attribute: 'name',
+                                        value: item.value
+                                    }
+                                ]
+                            };
+                        }
+                        return item;
+                    });
 
-      if (!$header.length) {
-        return;
-      }
+                    // console.log("Dữ liệu lọc đã biến đổi:", JSON.stringify(this.where));
+                }
 
-      // Vérifier si le bouton existe déjà
-      if (this.$el.find('.action-export-date-range').length) {
-        return;
-      }
+                return originalFetch(options);
+            };
+        },
 
-      // Créer le bouton
-      const $button = $('<button>')
-        .addClass('btn btn-default action-export-date-range')
-        .attr('type', 'button')
-        .css('margin-right', '5px')
-        .html('<span class="fas fa-file-export"></span> ' + this.translate('Export by Date Range', 'labels', 'CShipment'));
+        // Giữ lại các hàm cũ của bạn bên dưới
+        afterRender: function () {
+            Dep.prototype.afterRender.call(this);
+            this.addExportButton();
+        },
 
-      // Ajouter l'événement click
-      $button.on('click', () => {
-        this.actionExportByDateRange();
-      });
+        addExportButton: function () {
+            const $header = this.$el.find('.list-buttons-container');
+            if (!$header.length || this.$el.find('.action-export-date-range').length) return;
 
-      // Insérer le bouton avant le bouton Create
-      $header.prepend($button);
-    },
+            const $button = $('<button>')
+                .addClass('btn btn-default action-export-date-range')
+                .attr('type', 'button')
+                .css('margin-right', '5px')
+                .html('<span class="fas fa-file-export"></span> ' + this.translate('Export by Date Range', 'labels', 'CShipment'));
 
-    actionExportByDateRange: function () {
-      this.createView('dialog', 'custom:views/c-shipment/modals/export-date-range', {
-        scope: this.scope
-      }, (view) => {
-        view.render();
+            $button.on('click', () => { this.actionExportByDateRange(); });
+            $header.prepend($button);
+        },
 
-        this.listenToOnce(view, 'export', (data) => {
-          this.exportByDateRange(data.dateFrom, data.dateTo, data.format);
-        });
-      });
-    },
+        actionExportByDateRange: function () {
+            this.createView('dialog', 'custom:views/c-shipment/modals/export-date-range', {
+                scope: this.scope
+            }, (view) => {
+                view.render();
+                this.listenToOnce(view, 'export', (data) => {
+                    this.exportByDateRange(data.dateFrom, data.dateTo, data.format);
+                });
+            });
+        },
 
-    exportByDateRange: function (dateFrom, dateTo, format) {
-      Espo.Ui.notify(this.translate('pleaseWait', 'messages'));
+        exportByDateRange: function (dateFrom, dateTo, format) {
+            Espo.Ui.notify(this.translate('pleaseWait', 'messages'));
+            const where = [];
+            if (dateFrom && dateTo) {
+                where.push({ type: 'between', attribute: 'createdAt', value: [dateFrom, dateTo], dateTime: true });
+            } else if (dateFrom) {
+                where.push({ type: 'on', attribute: 'createdAt', value: dateFrom, dateTime: true });
+            } else if (dateTo) {
+                where.push({ type: 'on', attribute: 'createdAt', value: dateTo, dateTime: true });
+            }
 
-      // Créer les filtres de date au format EspoCRM pour createdAt
-      const where = [];
-
-      if (dateFrom) {
-        where.push({
-          type: 'on',
-          attribute: 'createdAt',
-          value: dateFrom,
-          dateTime: true
-        });
-      }
-
-      if (dateTo) {
-        where.push({
-          type: 'on',
-          attribute: 'createdAt',
-          value: dateTo,
-          dateTime: true
-        });
-      }
-
-      // Si les deux dates sont présentes, utiliser between
-      if (dateFrom && dateTo) {
-        where.length = 0; // Vider le tableau
-        where.push({
-          type: 'between',
-          attribute: 'createdAt',
-          value: [dateFrom, dateTo],
-          dateTime: true
-        });
-      }
-
-      const searchParams = {
-        where: where
-      };
-
-      // Utiliser l'API d'export native d'EspoCRM
-      Espo.Ajax.postRequest('Export', {
-        entityType: this.scope,
-        searchParams: searchParams,
-        attributeList: null, // Toutes les colonnes
-        format: format || 'xlsx'
-      }).then((result) => {
-        if (result.id) {
-          // Télécharger le fichier
-          window.location = this.getBasePath() + '?entryPoint=download&id=' + result.id;
-          Espo.Ui.success(this.translate('Done'));
+            Espo.Ajax.postRequest('Export', {
+                entityType: this.scope,
+                searchParams: { where: where },
+                attributeList: null,
+                format: format || 'xlsx'
+            }).then((result) => {
+                if (result.id) {
+                    window.location = this.getBasePath() + '?entryPoint=download&id=' + result.id;
+                    Espo.Ui.success(this.translate('Done'));
+                }
+            }).catch(() => {
+                Espo.Ui.error(this.translate('Error occurred'));
+            });
         }
-      }).catch(() => {
-        Espo.Ui.error(this.translate('Error occurred'));
-      });
-    }
-  });
+    });
 });
--- data/espocrm-app/data/cache/application/modules.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'Crm' => [
-    'order' => 10,
-    'jsTranspiled' => true
-  ]
-];
